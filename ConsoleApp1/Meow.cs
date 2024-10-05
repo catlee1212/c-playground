2d923f958e37995e353cdc14f1e56e04508c4467
Marlene Wiesmair <marlene.wiesmair@triceonic.com>
2024-10-05 11:05:07 +0200

    Fist console app.

---
+++ ConsoleApp1/Meow.cs
@@ -0,0 +1,42 @@
+﻿
+
+
+using System;
+namespace ConsoleApp1
+{
+  class Meow
+  {
+    static void Main(string[] args)
+    {
+
+      string catFace = @"
+    /\_____/\
+   /  o   o  \
+  ( ==  ^  == )
+   )         (
+  (           )
+ ( (  )   (  ) )
+(__(__)___(__)__)";
+
+      Console.WriteLine("Do you like cats?");
+      Console.WriteLine("Choose an option from the following list:");
+      Console.WriteLine("yes");
+      Console.WriteLine("no");
+      Console.WriteLine();
+
+      switch (Console.ReadLine())
+      {
+        case "yes":
+          Console.WriteLine("MEOW");
+          Console.WriteLine();
+          Console.WriteLine(catFace);
+          break;
+        case "no":
+          Console.WriteLine("Sorry this is not an option.");
+          break;
+
+      }
+    }
+  }
+
+}
